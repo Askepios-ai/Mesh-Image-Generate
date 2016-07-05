43f0b3e677f172c6b52063e6cf59c3107b47bfc6
Gerathain <mark@the-ellwoods.org>
2016-07-05 16:56:42 +0100

    massive speed improvement Managed to make a large improvement to the speed of the code by combining the functions to calculate a polygon colour and its difference. Now, when the colour is calculated, a list of pixels in the polygon is stored and this is used to calculate the difference The amount a node moves now scales with the scale of difference between best and worst node

--- mesh.py
+++ mesh.py
@@ -8,15 +8,24 @@ HEIGHT = 500
 
 random.seed()
 
+#########################################################################################
+##          Draws a polygon
+#########################################################################################
 def drawPolygon(poly, colour, drw):
     drw.polygon(poly, colour)
 
+#########################################################################################
+##          Draws all the nodes
+#########################################################################################
 def drawNodeList(nodeList, polyColourDict, drw):
     for x in range(0, NUM_NODES_WIDE - 1):
         for y in range(0, NUM_NODES_HIGH - 1):
             drawPolygon([nodeList[x][y], nodeList[x][y + 1], nodeList[x + 1][y + 1]], polyColourDict[tuple(sorted([(x, y), (x, y + 1), (x + 1, y + 1)], key=lambda x: (x[0], x[1])))], drw)
             drawPolygon([nodeList[x][y], nodeList[x + 1][y], nodeList[x + 1][y + 1]], polyColourDict[tuple(sorted([(x, y), (x + 1, y), (x + 1, y + 1)], key=lambda x: (x[0], x[1])))], drw)
 
+#########################################################################################
+##          Checks of points p1 and p2 are the same side of line a->b
+#########################################################################################
 #We can tell if the two points p1 and p2 are on the same side of the line a->b using cross products. (a->b)x(a->p1) should have the same z direction as (a->b)x(a->p2)
 def sameSide(a, b, p1, p2):
     ab = [a[0] - b[0], a[1] - b[1]] #get vector a->b
@@ -30,12 +39,18 @@ def sameSide(a, b, p1, p2):
     else:
         return False
 
+#########################################################################################
+##          Checks if a pixels is in a polygon
+#########################################################################################
 def pixelInPolygon(nodes, pixel):
     if sameSide(nodes[0], nodes[1], nodes[2], pixel) and sameSide(nodes[0], nodes[2], nodes[1], pixel) and sameSide(nodes[2], nodes[1], nodes[0], pixel):
         return True
     else:
         return False
 
+#########################################################################################
+##          Gets the smallest box that surrounds a polygon
+#########################################################################################
 def getPolyBox(poly):
     maxX = 0
     maxY = 0
@@ -54,6 +69,9 @@ def getPolyBox(poly):
             minY = node[1]
     return minX, maxX, minY, maxY
     
+#########################################################################################
+##          Calculates the colour a polygon should be
+#########################################################################################
 def calculatePolyColour(nodes, pix): #List of tuples of pixel coordinates, image pixels
     maxX = 0
     maxY = 0
@@ -64,6 +82,7 @@ def calculatePolyColour(nodes, pix): #List of tuples of pixel coordinates, image
     blueAverage = 0
     greenAverage = 0
 
+    pixelList = []
     numPixels = 0
     
     minX, maxX, minY, maxY = getPolyBox(nodes)
@@ -71,18 +90,28 @@ def calculatePolyColour(nodes, pix): #List of tuples of pixel coordinates, image
     for x in range(minX, maxX):
         for y in range(minY, maxY):
             if pixelInPolygon(nodes, [x, y]):
+		pixelList.append([x, y])
                 redAverage += pix[x, y][0]
                 blueAverage += pix[x, y][1]
                 greenAverage += pix[x, y][2]
                 numPixels += 1
-                
+
     if numPixels > 0:
         redAverage /= numPixels
         blueAverage /= numPixels
         greenAverage /= numPixels
 
-    return (redAverage, blueAverage, greenAverage)
+    difference = 0
+    for pixel in pixelList:
+        difference += abs(pix[pixel[0], pixel[1]][0] - redAverage)
+        difference += abs(pix[pixel[0], pixel[1]][1] - blueAverage)
+        difference += abs(pix[pixel[0], pixel[1]][2] - greenAverage)
 
+    return (redAverage, blueAverage, greenAverage, difference)
+
+#########################################################################################
+##          Returns a list of node references connected to provided node
+#########################################################################################
 def getPolyList(x, y): #In: grid ref, out: list of tuple of grid references
     return [((x, y), (x - 1, y - 1), (x - 1, y)),
             ((x, y), (x, y - 1), (x - 1, y - 1)),
@@ -91,27 +120,13 @@ def getPolyList(x, y): #In: grid ref, out: list of tuple of grid references
             ((x, y), (x + 1, y + 1), (x, y + 1)),
             ((x, y), (x, y + 1), (x - 1, y))]
 
-def getPolyColourDif(poly, realPoly, polyColourDict, pix): #In: List of tuple of grid ref, colour dictionary, image pixels
-    minX, maxX, minY, maxY = getPolyBox(realPoly)
-    difference = 0
-    numPixels = 0
 
-    for x in range(minX, maxX):
-        for y in range(minY, maxY):
-            if pixelInPolygon(realPoly, [x, y]):
-                numPixels += 1
-                difference += abs(pix[x, y][0] - polyColourDict[tuple(sorted(poly, key=lambda x: (x[0], x[1])))][0])
-                difference += abs(pix[x, y][1] - polyColourDict[tuple(sorted(poly, key=lambda x: (x[0], x[1])))][1])
-                difference += abs(pix[x, y][2] - polyColourDict[tuple(sorted(poly, key=lambda x: (x[0], x[1])))][2])
-
-
-    if numPixels > 0:
-        difference /= float(3*numPixels)
-
-    return difference
-
+#########################################################################################
+##          Move a node
+#########################################################################################
 def moveNode(x, y, nodeList, polyColourDict, pix):
     worstDif = 0
+    bestDif = 100000
     worstPoly = ()
     worstRealPoly = ()
     polyList = getPolyList(x, y)
@@ -119,15 +134,20 @@ def moveNode(x, y, nodeList, polyColourDict, pix):
     for poly in polyList:
         realPoly = (nodeList[poly[0][0]][poly[0][1]], nodeList[poly[1][0]][poly[1][1]], nodeList[poly[2][0]][poly[2][1]])
 	realPolyList.append(realPoly)
-        dif = getPolyColourDif(poly, realPoly, polyColourDict, pix)
+	dif = polyColourDict[tuple(sorted(poly, key=lambda x: (x[0], x[1])))][3]
+        if dif < bestDif:
+	    bestDif = dif
         if dif > worstDif:
             worstPoly = poly
             worstRealPoly = realPoly
             worstDif = dif
-
+    
+    scaleFactor = worstDif / max(bestDif, 1)
+    scaleFactor = max(min(scaleFactor, 4), 2)
+    scaleFactor = 6 - scaleFactor
     if worstDif > 0:
 	midpoint = ((worstRealPoly[0][0] + worstRealPoly[1][0] + worstRealPoly[2][0]) / 3, (worstRealPoly[0][1] + worstRealPoly[1][1] + worstRealPoly[2][1]) / 3)
-        nodeList[x][y] = ((nodeList[x][y][0] + ((midpoint[0] - nodeList[x][y][0]) / 3)), (nodeList[x][y][1] + ((midpoint[1] - nodeList[x][y][1]) / 3)))
+        nodeList[x][y] = ((nodeList[x][y][0] + ((midpoint[0] - nodeList[x][y][0]) / scaleFactor)), (nodeList[x][y][1] + ((midpoint[1] - nodeList[x][y][1]) / scaleFactor)))
 
 
     for poly in polyList:
